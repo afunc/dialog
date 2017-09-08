d78bd0dcef4e70163f9dc14bf22248ca78f5a392
紫紫 <Q157596462@outlook.com>
2017-09-08 15:12:59 +0800

    0

--- app/src/main/java/org/afunc/androiddemo/MainActivity.java
+++ app/src/main/java/org/afunc/androiddemo/MainActivity.java
@@ -1,8 +1,13 @@
 package org.afunc.androiddemo;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import org.afunc.android.mvp.RequirePresenter;
+import org.afunc.android.util.DimenUtils;
+import org.afunc.android.util.LogUtils;
 
 @RequirePresenter(MainPresenter.class)
 public class MainActivity extends AppActivity<MainPresenter> {
@@ -11,7 +16,7 @@ public class MainActivity extends AppActivity<MainPresenter> {
 
     @Override
     protected int setContentViewId() {
-        return R.layout.activity_loading;
+        return R.layout.activity_main;
     }
 
 
@@ -27,23 +32,39 @@ public class MainActivity extends AppActivity<MainPresenter> {
     }
 
 
-    //    @Override
-//    protected void onCreateAfterSuper() {
-//        super.onCreateAfterSuper();
-//        textView = $(R.id.title);
-//        textView.setText("title");
-//        msg = $(R.id.msg);
-//        msg.setText(String.format("%d + %d = %d", 1, 2, getPresenter().sum(1, 2)));
-//        msg.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                LogUtils.e("点击了");
+    @Override
+    protected void onCreateAfterSuper() {
+        super.onCreateAfterSuper();
+        textView = $(R.id.title);
+        textView.setText("title");
+        msg = $(R.id.msg);
+        msg.setText(String.format("%d + %d = %d", 1, 2, getPresenter().sum(1, 2)));
+        msg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogUtils.e("点击了");
 //                SweetAlertDialog alertDialog = new SweetAlertDialog(MainActivity.this, SweetAlertDialog.ERROR_TYPE);
 //                alertDialog.showCancelButton(true);
 //                showDialog(alertDialog);
-//            }
-//        });
-//    }
+
+                int size = DimenUtils.dp2px(MainActivity.this, 16);
+                ProgressBar progressBar = new ProgressBar(MainActivity.this);
+                progressBar.setPadding(size, size, size, size);
+                progressBar.setBackgroundResource(android.R.color.transparent);
+                String msg = "Text";
+                AlertDialog mLoadingDialog = new AlertDialog.Builder(MainActivity.this)
+                        .setView(progressBar)
+                        .setMessage(msg)
+//                    .setCancelable(false)
+                        .create();
+
+                mLoadingDialog.setCanceledOnTouchOutside(false);
+                showLoadingDialog(mLoadingDialog);
+
+
+            }
+        });
+    }
 
     // 互相调用
     protected int sum(int a, int b) {
