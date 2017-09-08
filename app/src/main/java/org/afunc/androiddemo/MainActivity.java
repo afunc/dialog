e431f42ddd8d0e093fa10fb858c86f91232e0f6e
紫紫 <Q157596462@outlook.com>
2017-09-08 19:16:13 +0800

    0

--- app/src/main/java/org/afunc/androiddemo/MainActivity.java
+++ app/src/main/java/org/afunc/androiddemo/MainActivity.java
@@ -1,13 +1,12 @@
 package org.afunc.androiddemo;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import org.afunc.android.mvp.RequirePresenter;
 import org.afunc.android.util.DimenUtils;
 import org.afunc.android.util.LogUtils;
+import org.afunc.weiget.loader.IndicatorLoader;
 
 @RequirePresenter(MainPresenter.class)
 public class MainActivity extends AppActivity<MainPresenter> {
@@ -41,33 +40,41 @@ public class MainActivity extends AppActivity<MainPresenter> {
         msg.setText(String.format("%d + %d = %d", 1, 2, getPresenter().sum(1, 2)));
         msg.setOnClickListener(new View.OnClickListener() {
             @Override
+            @SuppressWarnings("unchecked")
             public void onClick(View v) {
                 LogUtils.e("点击了");
 //                SweetAlertDialog alertDialog = new SweetAlertDialog(MainActivity.this, SweetAlertDialog.ERROR_TYPE);
 //                alertDialog.showCancelButton(true);
 //                showDialog(alertDialog);
 
-                int size = DimenUtils.dp2px(MainActivity.this, 16);
-                ProgressBar progressBar = new ProgressBar(MainActivity.this);
-                progressBar.setPadding(size, size, size, size);
-                progressBar.setBackgroundResource(android.R.color.transparent);
-                String msg = "Text";
-                AlertDialog mLoadingDialog = new AlertDialog.Builder(MainActivity.this)
-                        .setView(progressBar)
-                        .setMessage(msg)
-//                    .setCancelable(false)
-                        .create();
-
-                mLoadingDialog.setCanceledOnTouchOutside(false);
-                showLoadingDialog(mLoadingDialog);
+                int size = DimenUtils.dp2px(MainActivity.this, 50);
+//                ProgressBar progressBar = new ProgressBar(MainActivity.this);
+//                progressBar.setPadding(size, size, size, size);
+//                progressBar.setBackgroundResource(android.R.color.transparent);
 
 
+//                IndicatorView indicator = new IndicatorView(MainActivity.this);
+//                indicator.setmIndicatorColor(Color.parseColor("#FF00FF"));
+//                indicator.setmIndicatorId(IndicatorView.SemiCircleSpin);
+//                AlertDialog mLoadingDialog = new AlertDialog.Builder(MainActivity.this)
+//                        .setView(indicator).setMessage("123").setTitle("title")
+////                    .setCancelable(false)
+//                        .create();
+//                mLoadingDialog.setCanceledOnTouchOutside(false);
+//                showLoadingDialog(mLoadingDialog);
+
+                IndicatorLoader dialog = new IndicatorLoader(MainActivity.this);
+                dialog.getIndicator().setRandomIndicator(true);
+//                dialog.setTitle("asdasd3");
+
+                dialog.setBackgroundResource(R.drawable.corners_indicator);
+                showDialog(dialog);
             }
         });
     }
 
     // 互相调用
-    protected int sum(int a, int b) {
+    int sum(int a, int b) {
         return a + b;
     }
 }
